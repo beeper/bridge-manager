3745b28228f1fccb9746d6ce3dd46ebc43f2edb3
Tulir Asokan <tulir@maunium.net>
2023-08-20 20:17:37 +0300

    Allow specifying env in dockerfile

--- docker/run-bridge.sh
+++ docker/run-bridge.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -euf -o pipefail
 if [[ -z "$BRIDGE_NAME" ]]; then
 	echo "BRIDGE_NAME not set"
 	exit 1
 fi
 export BBCTL_CONFIG=${BBCTL_CONFIG:-/bbctl.json}
+export BEEPER_ENV=${BEEPER_ENV:-prod}
 if [[ ! -f $BBCTL_CONFIG ]]; then
 	if [[ -z "$MATRIX_ACCESS_TOKEN" ]]; then
 		echo "MATRIX_ACCESS_TOKEN not set"
@@ -16,6 +18,6 @@ if [[ ! -f $BBCTL_CONFIG ]]; then
 	fi
 	export DB_DIR=${DB_DIR:-/data/db}
 	mkdir -p $DB_DIR
-	jq -n '{environments: {prod: {access_token: env.MATRIX_ACCESS_TOKEN, database_dir: env.DB_DIR, bridge_data_dir: env.DATA_DIR}}}' > $BBCTL_CONFIG
+	jq -n '{environments: {"\(env.BEEPER_ENV)": {access_token: env.MATRIX_ACCESS_TOKEN, database_dir: env.DB_DIR, bridge_data_dir: env.DATA_DIR}}}' > $BBCTL_CONFIG
 fi
-bbctl run $BRIDGE_NAME
+bbctl -e $BEEPER_ENV run $BRIDGE_NAME
